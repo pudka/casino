8f69a60b8af7fe3f996d3ed7d2ee98365c93bffa
pudka <pudgeinvonyx@gmail.com>
2021-04-09 19:25:51 +0300

    Add new routs

--- run.py
+++ run.py
@@ -18,5 +18,30 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/vacancy')
+def vacancy():
+    return render_template("vacancy.html")
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template("contacts.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(port=8888, debug=True)
