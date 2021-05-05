c149bd51a45cd27c440422a6730e41342cd08f58
pudka <pudgeinvonyx@gmail.com>
2021-05-06 01:25:52 +0300

    save state

--- casino_app/static/js/script.js
+++ casino_app/static/js/script.js
@@ -11,3 +11,66 @@ jQuery('.link-details').click(function () {
 	jQuery(this).toggle();
 	jQuery(this).next().toggle();
 });
+
+
+// set up google map
+function initMap() {
+	// Map options
+    var options = {
+        zoom: 8,
+        center: { lat: 50.431759, lng: 30.517023 },
+    }
+
+    // New map
+    var map = new
+    google.maps.Map(document.getElementById('map'), options);
+
+    /*
+    // Add marker
+    var marker = new google.maps.Marker({
+    	position:{lat: 50.431759, lng: 30.517023},
+    	map: map
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+    	content:'Casino Royal'
+    });
+
+    marker.addListener('click', function(){
+    	infoWindow.open(map, marker);
+    })
+	*/
+	addMarker({
+		coords: {lat: 50.431759, lng: 30.517023},
+		content: '<h6>Kyiv, Cyber Slots</h3>'
+	});
+	addMarker({
+		coords: {lat: 49.842957, lng: 24.031111},
+		content: '<h6>Lviv, Cyber Slots</h3>'
+	});
+	addMarker({
+		coords:{lat: 49.988358, lng: 36.232845},
+		content: '<h6>Kharkiv, Cyber Slots</h3>'
+	});
+
+    function addMarker(props) {
+    	var marker = new google.maps.Marker({
+	    	position:props.coords,
+	    	map: map
+	    });
+
+	    if (props.content) {
+			var infoWindow = new google.maps.InfoWindow({
+		    	content: props.content
+		    });
+
+		    marker.addListener('click', function(){
+		    	infoWindow.open(map, marker);
+		    });
+	    }
+
+	    if (props.iconImage) {
+
+	    }
+    }
+}
