d9d3dfa3c0b17aa6346dffad25058e8d80a536ec
pudka <pudgeinvonyx@gmail.com>
2021-05-11 13:34:08 +0300

    markerk failed.

--- casino_app/static/js/script.js
+++ casino_app/static/js/script.js
@@ -17,26 +17,51 @@ jQuery('.link-details').click(function () {
 function initMap() {
 	// Map options
     var options = {
-        zoom: 4,
-        center: { lat: 50.431759, lng: 30.517023 },
+        zoom: 6,
+        center: { lat: 48.383022, lng: 31.1828699 },
     }
 
     // New map
     var map = new
     google.maps.Map(document.getElementById('map'), options);
 
-	addMarker({
-		coords: {lat: 50.431759, lng: 30.517023},
-		content: '<h6>Kyiv, Cyber Slots</h3>'
-	});
-	addMarker({
-		coords: {lat: 49.842957, lng: 24.031111},
-		content: '<h6>Lviv, Cyber Slots</h3>'
-	});
-	addMarker({
-		coords:{lat: 49.988358, lng: 36.232845},
-		content: '<h6>Kharkiv, Cyber Slots</h3>'
-	});
+    setInterval(getLocation, 5000);
+
+	function getLocation() {
+		$.post('http://0.0.0.0:51413/contacts', {}, function(){
+	    	var select_location = document.getElementById('select_location');
+	    	location = select_location.options[select_location.selectedIndex].value == 'all';
+
+	    	if ( location == 'all' ) {
+		    	addMarker({
+					coords: {lat: 48.51649271602329, lng: 32.26452466947545},
+					content: '<p>Cyber Slots in Zirka Hotel, Kyiv</p>'
+				});
+
+				addMarker({
+					coords: {lat: 50.44671781005228, lng: 30.50906041372692},
+					content: '<p>Cyber Slots in Royal Grand Hotel, Kyiv</p>'
+				});
+
+				addMarker({
+					coords: {lat: 46.47128011885416, lng: 30.7385034405681},
+					content: '<h6>Cyber Slots in Zenith Hotel, Odessa</h3>'
+				});
+		    }
+
+		    if (location == 'Kyivska') {
+		    	addMarker({
+					coords: {lat: 48.51649271602329, lng: 32.26452466947545},
+					content: '<p>Cyber Slots in Zirka Hotel, Kyiv</p>'
+				});
+
+				addMarker({
+					coords: {lat: 50.44671781005228, lng: 30.50906041372692},
+					content: '<p>Cyber Slots in Royal Grand Hotel, Kyiv</p>'
+				});
+		    }
+		});
+    }
 
     function addMarker(props) {
     	var marker = new google.maps.Marker({
